d554088fb583322e0c7c3abf47a334af51f0cc05
hanbitgoun <akdntm946@naver.com>
2021-07-21 10:53:08 +0900

    20210721_w commit_2_2

--- acountapp/views.py
+++ acountapp/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from acountapp.decorators import account_ownership_required
 from acountapp.forms import AccountCreationForm
 from acountapp.models import HelloWorld
 
@@ -46,8 +47,10 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'acountapp/detail.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+has_ownership = [login_required, account_ownership_required]
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -55,8 +58,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('acountapp:hello_world')
     template_name = 'acountapp/update.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
