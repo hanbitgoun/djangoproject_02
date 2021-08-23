b557aebf2d4dca5b33219e3688bb8ee7b5f8b26b
hanbitgoun <akdntm946@naver.com>
2021-08-23 15:11:44 +0900

    20210823 subscribe list

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscribeListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
     path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscribeListView.as_view(), name='list'),
 
 ]
