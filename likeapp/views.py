e6bceba61369b056a4cbeb2a7b2dc49aa1f7700b
hanbitgoun <akdntm946@naver.com>
2021-08-23 16:24:11 +0900

    20210823 like_3

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import RedirectView
+
+from articleapp.models import Article
+from likeapp.models import LikeRecord
+
+
+class LikeArticleView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        article = Article.objects.get(pk=kwargs['article_pk'])
+
+        # 좋아요 기록
+        likerecord = LikeRecord.objects.filter(user=user, article=article)
+
+        if likerecord.exists():
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
+        else:
+            LikeRecord(user=user, article=article).save()
+
+        article.like += 1
+        article.save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']})
